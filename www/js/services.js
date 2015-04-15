4d56352ef76e99e74d3cc5e2b4d53613e3393e5b
mehakdua <mdua798@gmail.com>
2015-04-15 16:10:35 +0530

    add service for pregnancy detail

--- www/js/services.js
+++ www/js/services.js
@@ -47,4 +47,68 @@ angular.module('starter.services', [])
       return null;
     }
   };
+})
+.factory('UtilityService',function(){
+  var cityCode="500", slumCode="200", workerCode="100";
+
+  return {
+    getSlumCode: function(){
+      return slumCode;
+    },
+    getCityCode: function(){
+      return cityCode;
+    },
+    getWorkerCode: function(){
+
+    },
+    calcAge: function(dob){
+      //get age from 
+    },
+    generateWomanID: function(houseCode, womanNo){
+      return cityCode+slumCode+workerCode+houseCode+womanNo;
+    },
+    showMonth:function(monthNo){
+
+        switch(monthNo) {
+        case 0:
+            return "Jan"
+            break;
+        case 1:
+            return "Feb"
+            break;
+        case 2:
+            return "Mar"
+            break;
+        case 3:
+            return "Apr"
+            break;
+        case 4:
+            return "May"
+            break;
+        case 5:
+            return "Jun"
+            break;
+        case 6:
+            return "Jul"
+            break;
+        case 7:
+            return "Aug"
+            break;   
+        case 8:
+            return "Sep"
+            break;
+        case 9:
+            return "Oct"
+            break;
+        case 10:
+            return "Nov"
+            break;
+        case 11:
+            return "Dec"
+            break;
+        default:
+            return "Jan"
+      }
+    }
+  }
 });
