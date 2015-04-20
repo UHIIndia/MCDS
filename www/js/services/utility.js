f52f9a7d018ba7f0dc2622eed45ed62180feda98
Kamlesh Kumar Jha <kamleshkrjha.87@gmail.com>
2015-04-20 12:46:35 +0530

    add camera in add woman feature

--- www/js/services/utility.js
+++ www/js/services/utility.js
@@ -1,70 +1,70 @@
 angular.module('starter.services')
-.factory('UtilityService',function(){
-    var cityCode="500", slumCode="200", workerCode="100";
+.factory('UtilityService',['$cordovaFile', '$cordovaCamera', function($cordovaFile, $cordovaCamera){
+  var cityCode="500", slumCode="200", workerCode="100";
+ var dataDir, folder="images";  
+  return {
+    alertMessages: {
+      liveBirth: 'Live births can not be greater than total pregnencies',
+      livingChildren: 'Living Children can not be greater than number of children birth alive'
+    },
+    getSlumCode: function(){
+      return slumCode;
+    },
+    getCityCode: function(){
+      return cityCode;
+    },
+    getWorkerCode: function(){
+     return workerCode;
+    },
+    calcAge: function(dob, inYear){
+        var currDate =new Date(),
+        diffYear = currDate.getFullYear() - dob.getFullYear(),
+        diffMonth = currDate.getMonth() - dob.getMonth();
+        if(inYear){
+          // age is calculated in years 
+          return diffYear;
+        } else{
+          //age is Calculated in months
+          return 12 * diffYear + diffMonth;       
 
-    return {
-        alertMessages: {
-            liveBirth: 'Live births can not be greater than total pregnencies',
-            livingChildren: 'Living Children can not be greater than number of children birth alive'
-        },
-        getSlumCode: function(){
-            return slumCode;
-        },
-        getCityCode: function(){
-            return cityCode;
-        },
-        getWorkerCode: function(){
-            return workerCode;
-        },
-        calcAge: function(dob, inYear){
-            var currDate =new Date(),
-                diffYear = currDate.getFullYear() - dob.getFullYear(),
-                diffMonth = currDate.getMonth() - dob.getMonth();
-            if(inYear){
-                // age is calculated in years
-                return diffYear;
-            } else{
-                //age is Calculated in months
-                return 12 * diffYear + diffMonth;
-
-            }
-
-        },
-        calcDob : function(age,inYear){
-            var currDate= new Date(),
-                currYear=currDate.getFullYear(),
-                currMonth =currDate.getMonth() ;
-            if(inYear){
-                //return new Date("Jan,15,"+currYear - age);
-                return new Date(currYear-age +"-01-15");//
-            } else {
-                //age value is in months
-                var year = parseInt(age/12), month=age%12, monthDiff = currMonth - month;
-                if(monthDiff>0){
-                    return new Date(currYear - year+"-"+monthDiff+"-15");
-                } else{
-                    return new Date(currYear - year - 1+"-"+(12+monthDiff)+"-15");
-                }
-            }
-
-        },
-        addDaysToDate : function(dateObj, days){
-            var dat = new Date(dateObj.valueOf());
-            dat.setDate(dat.getDate() + days);
-            return dat;
-        },
-        subtractDaysFromDate : function(dateObj, days){
-            var dat = new Date(dateObj.valueOf());
-            dat.setDate(dat.getDate() - days);
-            return dat;
-        },
-        isSameDate : function(date1, date2){
-            //only  checking day month and year
-            return (date1.getFullYear() == date2.getFullYear()) && (date1.getMonth() == date2.getMonth()) && (date1.getDate() == date2.getDate());
-        },
-        generateWomanID: function(houseCode, womanNo){
-            return cityCode+slumCode+workerCode+houseCode+womanNo;
-        },
+        }       
+    
+    },
+    calcDob : function(age,inYear){
+      var currDate= new Date(),
+      currYear=currDate.getFullYear(),
+      currMonth =currDate.getMonth() ;      
+      if(inYear){
+        //return new Date("Jan,15,"+currYear - age);
+        return new Date(currYear-age +"-01-15");//
+      } else {
+        //age value is in months
+        var year = parseInt(age/12), month=age%12, monthDiff = currMonth - month;
+        if(monthDiff>0){
+          return new Date(currYear - year+"-"+monthDiff+"-15");
+        } else{
+          return new Date(currYear - year - 1+"-"+(12+monthDiff)+"-15");
+        }
+      }
+            
+    },
+    addDaysToDate : function(dateObj, days){
+      var dat = new Date(dateObj.valueOf());
+      dat.setDate(dat.getDate() + days);
+      return dat;
+    }, 
+    subtractDaysFromDate : function(dateObj, days){
+      var dat = new Date(dateObj.valueOf());
+      dat.setDate(dat.getDate() - days);
+      return dat;
+    },
+    isSameDate : function(date1, date2){
+      //only  checking day month and year
+      return (date1.getFullYear() == date2.getFullYear()) && (date1.getMonth() == date2.getMonth()) && (date1.getDate() == date2.getDate());
+    },
+    generateWomanID: function(houseCode, womanNo){
+      return cityCode+slumCode+workerCode+houseCode+womanNo;
+    },
         showMonthFromDate:function(date){
             var dateArray = date.split("/");
             return dateArray[1];
@@ -75,57 +75,143 @@ angular.module('starter.services')
             var year=date.getFullYear();
             return day+"/"+month+"/"+year;
         },
-        showMonth:function(monthNo){
+    showMonth:function(monthNo){
 
-            switch(monthNo) {
+        switch(monthNo) {
                 case 1:
                 case 01:
-                    return "Jan"
-                    break;
+            return "Jan"
+            break;
                 case 2:
                 case 02:
-                    return "Feb"
-                    break;
+            return "Feb"
+            break;
                 case 3:
                 case 03:
-                    return "Mar"
-                    break;
+            return "Mar"
+            break;
                 case 4:
                 case 04:
-                    return "Apr"
-                    break;
+            return "Apr"
+            break;
                 case 5:
                 case 05:
-                    return "May"
-                    break;
+            return "May"
+            break;
                 case 6:
                 case 06:
-                    return "Jun"
-                    break;
+            return "Jun"
+            break;
                 case 7:
                 case 07:
-                    return "Jul"
-                    break;
+            return "Jul"
+            break;
                 case 8:
                 case 08:
-                    return "Aug"
-                    break;
+            return "Aug"
+            break;   
                 case 9:
                 case 09:
-                    return "Sep"
-                    break;
+            return "Sep"
+            break;
                 case 10:
-                    return "Oct"
-                    break;
+            return "Oct"
+            break;
                 case 11:
-                    return "Nov"
-                    break;
+            return "Nov"
+            break;
                 case 12:
-                    return "Dec"
-                    break;
-                default:
-                    return "Jan"
-            }
-        }
+            return "Dec"
+            break;
+        default:
+            return "Jan"
+      }
+    },
+    captureImage: function(){
+     document.addEventListener("deviceready", function () {
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+
+    return $cordovaCamera.getPicture(options).then(function(imageData) { 
+    // alert(imageData);
+     return imageData;    
+    }, function(err) {
+      // error
+     alert("ERROR IN CAMERA CAPTURE: "+err.msg);
+     return;
+    });
+
+    }, false);
+    },
+    loadImage : function(fileName){
+     //fileName is {id}.jpeg
+     try{
+       var imgURL = cordova.file.externalDataDirectory + folder+"/"+fileName+".jpg";
+       return imgURL;
+     } catch(e){
+      alert("this is browser, set a default Image");
+      return;
+     }
+     
+    },
+    saveImage : function(imageData, fileName){ 
+    // alert("inside utility service");
+    try {
+         dataDir = cordova.file.externalDataDirectory;
+       //alert("ext dir: "+dataDir);
+      
+     
+     if(!dataDir){
+      console.log('image will be saved only on device');
+      return;
+     }
+     function fail(err){
+      alert("file error: "+err.message);
+     }
+     function onCopySuccess(){
+      alert('image file saved');
+     }
+     function copyFile(fileEntry){
+      var newName = fileName+".jpg";
+      window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory+folder, function(fileSystem2) {
+      // alert('ext dir for copying :'+ fileSystem2);
+       //kkj: use moveTo and then point to new saved image in extyernal directory
+				fileEntry.moveTo(
+					fileSystem2,
+					newName,
+					onCopySuccess,
+					fail
+				);
+			},
+			fail);
+     }
+     var onSuccessDir = function(success){
+      // copy file 
+     // alert("Directory is present ");
+      window.resolveLocalFileSystemURL(imageData, copyFile, fail);       
+      };
+     
+     $cordovaFile.checkDir(dataDir, folder).then(onSuccessDir, function(err){
+      //alert("No directory found, create new directory:"+ err.message);
+       $cordovaFile.createDir(dataDir, folder).then(onSuccessDir, function(err){
+        alert('Error in creating directory: '+err.message);
+       });
+      
+     });
+    } catch(e){
+     
     }
-});
+  
+    
+    }
+  }
+}]);
--- www/js/services/woman.js
+++ www/js/services/woman.js
@@ -1,59 +1,60 @@
 angular.module('starter.services')
 .factory('WomanService',['FileService','UtilityService', function(FileService, UtilityService){
-    var womenList = FileService.readFromLocalStorage(true);
-    // this function return a id i.e house#.womanNo , this will be visible to user
-    var generateVisibleWomanID = function(houseCode){
-        var womanNo=0; //  is the number of woman in a particular house
-        // find if this house is already present in the list
-        angular.forEach(womenList, function(womanObj, index){
-            if(womanObj.house == houseCode){
-                womanNo++;
-            }
-        });
-        return houseCode+"."+(womanNo+1);
-    };
-    // this function return a id i.e house#.womanNo , this will be visible to user
-    var generateActualWomanID = function(name, dob){
-        var nameCode, dobCode;
-        name ? nameCode = name.substring(0,2).toUpperCase() : nameCode = "NA";
-
-        return UtilityService.getCityCode()+UtilityService.getSlumCode()+UtilityService.getWorkerCode()+nameCode+dob.getDate()+dob.getMonth()+dob.getFullYear();
-    };
-    return {
-        isWomanRegistered: function(womanID){
-            // check if this woman is already in women list or not
-        },
-        getWomanDetails: function(womanID){
-            // get the details of woman from all women list
-            for(var i=0; i<womenList.length; i++){
-                if(womanID === womenList[i].visibleID){
-                    return womenList[i];
-                }
-            }
-        },
-        updateWomanDetails: function(womanObj){
-            // save woman related details to
-            var womanDetails = this.getWomanDetails(womanObj.womanID);
-            angular.forEach(womanObj, function(value, key){
-
-                womanDetails[key] = value;
-
-            });
-        },
-        addNewWoman: function (womanObj) {
-            var womanID = generateActualWomanID(womanObj.name, womanObj.dob);
-            var visibleID = generateVisibleWomanID(womanObj.house);
-            // add this woman to women List
-            womanObj.womanID = womanID;
-            womanObj.visibleID = visibleID;
-            womanObj.city =UtilityService.getCityCode();
-            womanObj.slum =UtilityService.getSlumCode();
-            womanObj.worker = UtilityService.getWorkerCode();
-            womanObj.registrationDate = new Date();
-            womenList.push(womanObj);
-            FileService.writeToLocalStorage(womenList, true);
-            console.log(womenList);
-            return womanID;
+  var womenList = FileService.readFromLocalStorage(true);
+  // this function return a id i.e house#.womanNo , this will be visible to user 
+  var generateVisibleWomanID = function(houseCode){
+    var womanNo=0; //  is the number of woman in a particular house
+       // find if this house is already present in the list
+       angular.forEach(womenList, function(womanObj, index){
+          if(womanObj.house == houseCode){
+            womanNo++;
+          }
+       });
+      return houseCode+"."+(womanNo+1);
+  };
+  // this function return a id i.e house#.womanNo , this will be visible to user 
+  var generateActualWomanID = function(name, dob){  
+   var nameCode, dd= dob.getDate(), mm=dob.getMonth()+1, yyyy= dob.getFullYear();
+   name ? nameCode = name.substring(0,2).toUpperCase() : nameCode = "NA";
+   if(dd< 10)  dd ="0"+ dd;;
+   if(mm < 10) mm ="0"+mm;
+      return UtilityService.getCityCode()+UtilityService.getSlumCode()+UtilityService.getWorkerCode()+nameCode+dd+mm+yyyy;
+  };
+  return {
+    isWomanRegistered: function(womanID){
+      // check if this woman is already in women list or not
+    },
+    getWomanDetails: function(womanID){
+      // get the details of woman from all women list
+      for(var i=0; i<womenList.length; i++){
+        if(womanID === womenList[i].visibleID){
+          return womenList[i];
         }
+      }
+    },
+    updateWomanDetails: function(womanObj){
+      // save woman related details to 
+      var womanDetails = this.getWomanDetails(womanObj.womanID);
+      angular.forEach(womanObj, function(value, key){
+        
+          womanDetails[key] = value;
+      
+      });
+    },
+    addNewWoman: function (womanObj) {
+      var womanID = generateActualWomanID(womanObj.name, womanObj.dob);
+      var visibleID = generateVisibleWomanID(womanObj.house);
+      // add this woman to women List
+      womanObj.womanID = womanID;
+      womanObj.visibleID = visibleID;
+      womanObj.city =UtilityService.getCityCode();
+      womanObj.slum =UtilityService.getSlumCode();
+      womanObj.worker = UtilityService.getWorkerCode();
+      womanObj.registrationDate = new Date();
+      womenList.push(womanObj);      
+      FileService.writeToLocalStorage(womenList, true);
+        console.log(womenList);
+        return womanID;      
     }
+  }
 }]);
