4c24420c3f0186ceca125b978edc72af80508743
Priya Ranjan Singh <mail@priyaranjan.net>
2015-04-21 01:02:49 +0530

    fetch existing basic information of woman for family planning

--- www/js/controllers/fp.js
+++ www/js/controllers/fp.js
@@ -1,4 +1,11 @@
-angular.module('starter.controllers')
-.controller('FpController', function($scope) {
+angular.module('starter.controllers').controller('FpController', function($scope, WomanService) {
+  var id = 'A-102.1';
+  $scope.woman = WomanService.getWomanDetails(id);
+
+  // add mock scope variables to be received from service
+  $scope.woman.ageYoungestChild = 1;
+  $scope.woman.genderYoungestChild = 'm';
+  $scope.woman.isPregnant = false;
+  $scope.woman.isBreastFeeding = false;
 
 });
