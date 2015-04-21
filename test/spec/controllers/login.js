2358b4598177a4d11fd0b8ae5f36d3cdb769b83a
Priya Ranjan Singh <mail@priyaranjan.net>
2015-04-22 00:23:35 +0530

    add unit tests for login controller

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -1,18 +1,40 @@
 'use strict';
 
 describe('Controller: LoginController', function(){
-  var scope;
+  var scope, location;
 
   // load the controller's module
   beforeEach(module('starter.controllers'));
 
-  beforeEach(inject(function($rootScope, $controller) {
+  beforeEach(inject(function($rootScope, $controller, _$location_) {
     scope = $rootScope.$new();
+    location = _$location_;
     $controller('LoginController', {$scope: scope});
   }));
 
-  // tests start here
-  it('should always be true', function(){
-    expect(true).toBe(true);
+  it('should set correct scope variables when right worker logs in', function(){
+    scope.workerCode = '123';
+    scope.enterHome();
+    expect(scope.required).toBe(false);
+    expect(scope.incorrect).toBe(false);
   });
+
+  it('should redirect to home on successful login', function(){
+    scope.workerCode = '123';
+    scope.enterHome();
+    expect(location.path()).toBe('/home');
+  });
+
+  it('should notify for missing mandatory field when worker code is not provided', function() {
+    scope.workerCode = '';
+    scope.enterHome();
+    expect(scope.required).toBe(true);
+  });
+
+  it('should notify for failed login when incorrect worker code is provided', function() {
+    scope.workerCode = '123456';
+    scope.enterHome();
+    expect(scope.incorrect).toBe(true);
+  });
+
 });
