d8b0ee447f8b7dac89ec9c073207c954a86c02e2
Priya Ranjan Singh <mail@priyaranjan.net>
2015-04-21 13:43:47 +0530

    add feature of selecting a family planning method

--- www/js/controllers/fp.js
+++ www/js/controllers/fp.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers').controller('FpController', function($scope, WomanService) {
+angular.module('starter.controllers').controller('FpController', function($scope, WomanService, familyPlanning) {
   var id = 'A-102.1';
   $scope.woman = WomanService.getWomanDetails(id);
 
@@ -7,5 +7,15 @@ angular.module('starter.controllers').controller('FpController', function($scope
   $scope.woman.genderYoungestChild = 'm';
   $scope.woman.isPregnant = false;
   $scope.woman.isBreastFeeding = false;
+
+  var familyPlanningMethods = familyPlanning.getFamilyPlanningMethods();
+
+  $scope.selectFPMethod = function(methodID) {
+     var filteredMethodList = familyPlanningMethods.filter(function(e) {
+      return e.id === methodID;
+    });
+    $scope.FPMethod = filteredMethodList[0];
+    console.log($scope.FPMethod);
+  };
 
 });
