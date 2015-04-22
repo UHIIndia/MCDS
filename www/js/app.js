ab8c6ecf9b4326e79d7808711202e1bf3c9bd0a6
Priya Ranjan Singh <mail@priyaranjan.net>
2015-04-22 09:07:27 +0530

    cleanup comments in module declaration

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
-// Ionic Starter App
+// Ionic App for uhi
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'uhiApp' is the name of this angular module
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('uhiApp', ['ionic', 'pascalprecht.translate', 'uhiApp.controllers', 'uhiApp.services', 'ngCordova', 'ui.bootstrap'])
+// 'uhiApp.controllers' is found in js/controllers.js
+// 'uhiApp.services' is found in js/services.js
+angular.module('uhiApp', ['ionic', 'ngCordova', 'uhiApp.controllers', 'uhiApp.services', 'pascalprecht.translate', 'ui.bootstrap'])
 
 
 .run(function($ionicPlatform) {
@@ -25,9 +25,6 @@ angular.module('uhiApp', ['ionic', 'pascalprecht.translate', 'uhiApp.controllers
 .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
   // setup the state for the login directive
