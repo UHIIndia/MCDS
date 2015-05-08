0f48e96b9a01fcb74f47b2df4ce9ce267ab8871d
Mehak Dua <mdua798@gmail.com>
2015-05-08 17:38:41 +0530

    add video gallery page

--- www/js/controllers/film-women.js
+++ www/js/controllers/film-women.js
@@ -1,3 +1,37 @@
-angular.module('uhiApp.controllers').controller('FilmWomenController', function($scope, $rootScope) {
+angular.module('uhiApp.controllers').controller('FilmWomenController', function($scope,$timeout, $rootScope,videos) {
 
+    $scope.video = {};
+    var allVideos=[]
+    $scope.tab;
+    var selectedList=[];
+    $scope.video.fpList = videos.getFamilyPlanningVideos();
+    $scope.video.anclist = videos.getANCVideos();
+    $scope.video.childlist = videos.getChildVideos();
+    allVideos = ($scope.video.fpList).concat($scope.video.childlist ,$scope.video.fpList);
+    $scope.video.all=allVideos;
+    selectedList = $scope.video.all;
+    $scope.selectVideoTab =function(tabIndex){
+      $scope.tab = tabIndex;
+      if(tabIndex == 0){
+        selectedList = $scope.video.all;
+      }else if(tabIndex == 1){
+        selectedList = $scope.video.fpList;
+      }else if(tabIndex == 2){
+        selectedList = $scope.video.anclist;
+      }else if(tabIndex == 3){
+        selectedList = $scope.video.childlist;
+      }
+    };
+    $scope.playVideo = function(id) {
+      var selectedVideo = selectedList[id];
+      $scope.showVideo = true;
+      $scope.videoPath = selectedVideo.path;
+      $timeout(function() {
+        document.getElementById('selected-video').play();
+      }, 1000);
+    };
+    $scope.stopVideo = function() {
+      document.getElementById('selected-video').pause();
+      $scope.showVideo= false;
+    };
 });
--- www/js/services/videos.js
+++ www/js/services/videos.js
@@ -19,135 +19,224 @@ angular.module('uhiApp.services').factory('videos',function($window){
     {
       'id': 1,
       'filename': 'fst.mp4',
-      'title': 'Female Sterilization'
+      'title': 'Female Sterilization',
+      'methodName':'Female Sterilization',
+      'methodMsg':'(Kaise apnayen mahila nasbandi)'
     },
     {
       'id': 2,
       'filename': 'nsv.mp4',
-      'title': 'Male Sterilization'
+      'title': 'Male Sterilization',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 3,
       'filename': 'iucd.mp4',
-      'title': 'IUCD'
+      'title': 'IUCD',
+      'methodName':'IUCD',
+      'methodMsg':'(kaise apnayen IUCD)'
     },
     {
       'id': 4,
       'filename': 'dmpa.mp4',
-      'title': 'DMPA'
+      'title': 'DMPA',
+      'methodName':'DMPA',
+      'methodMsg':'(kaise apnayen DMPA)'
     },
     {
       'id': 5,
       'filename': 'ocp.mp4',
-      'title': 'OCP'
+      'title': 'OCP',
+      'methodName':'OCP',
+      'methodMsg':'(kaise apnayen OCP)'
     },
     {
       'id': 6,
       'filename': 'condom.mp4',
-      'title': 'Condom'
+      'title': 'Condom',
+      'methodName':'Condom',
+      'methodMsg':'(kaise apnayen condom)'
     },
     {
       'id': 7,
       'filename': 'ppfp.mp4',
-      'title': 'Postpartum family planning'
+      'title': 'Postpartum family planning',
+      'methodName':'Postpartum family planning',
+      'methodMsg':'(kaise apnayen prasav paschat pariwar niyojan)'
     },
     {
       'id': 8,
       'filename': 'papf.mp4',
-      'title': 'Post abortion family planning'
+      'title': 'Post abortion family planning',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen garbhpat paschatpariwar niyojan)'
     }
   ];
 
    var ancVideos = [
     {
       'id': 1,
-      'filename': 'asha.mp4',
-      'title': 'ASHA AWWW VISIT'
+      'filename': 'anc.mp4',
+      'title': 'ASHA AWWW VISIT',
+      'methodName':'Get ANC checkups during pregnancy',
+      'methodMsg':'(Garbhawasta mein jaanch karvaayn)'
     },
     {
       'id': 2,
-      'filename': 'anm.mp4',
-      'title': 'ANM. DOC. CHECKUP'
+      'filename': 'IFA.mp4',
+      'title': 'ANM. DOC. CHECKUP',
+      'methodName':'Take IFA during pregnancy',
+      'methodMsg':'(Garbhawasta mein Iron ki goli khayen)'
     },
     {
       'id': 3,
-      'filename': 'weight.mp4',
-      'title': 'Weight'
+      'filename': 'tt.mp4',
+      'title': 'Weight',
+      'methodName':'Get tetanus toxoid during pregnancy',
+      'methodMsg':'(Garbhawasta mein tetanus ka tika lagvayen)'
     },
     {
       'id': 4,
-      'filename': 'tt.mp4',
-      'title': 'TT'
+      'filename': 'urineprotein.mp4',
+      'title': 'TT',
+      'methodName':'How to recognize danger signs during pregnancy',
+      'methodMsg':'(Garbhawasta ke dauran khatron ke lakshan pehchane)'
     },
     {
       'id': 5,
-      'filename': 'anaemia.mp4',
-      'title': 'Anaemia'
+      'filename': 'papf.mp4',
+      'title': 'Anaemia',
+      'methodName':'Prepare for your delivery',
+      'methodMsg':'(Prasav ki tayyari karein)'
     },
     {
       'id': 6,
-      'filename': 'paleness.mp4',
-      'title': 'Paleness'
+      'filename': 'deliveryinhospital.mp4',
+      'title': 'Paleness',
+      'methodName':'Deliver in a hospital',
+      'methodMsg':'(Aspatal mein hi prasav karaaein)'
     },
     {
       'id': 7,
-      'filename': 'nightblind.mp4',
-      'title': 'Night Blind'
-    },
+      'filename': 'ppfp.mp4',
+      'title': 'Night Blind',
+      'methodName':'Take care of yourself after delivery',
+      'methodMsg':'(Prasav ke bad apni dekhbhaal karein)'
+    }/*,
     {
       'id': 8,
       'filename': 'bleeding.mp4',
-      'title': 'Bleeding'
+      'title': 'Bleeding',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 9,
       'filename': 'malaria.mp4',
-      'title': 'Malaria'
+      'title': 'Malaria',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 10,
       'filename': 'ifaTablets.mp4',
-      'title': 'IFA Tablets'
+      'title': 'IFA Tablets',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 11,
       'filename': 'bp.mp4',
-      'title': 'BP'
+      'title': 'BP',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 12,
       'filename': 'urineprotein.mp4',
-      'title': 'Urine Protein'
+      'title': 'Urine Protein',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 13,
       'filename': 'swelling.mp4',
-      'title': 'Swelling'
+      'title': 'Swelling',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 14,
       'filename': 'fits.mp4',
-      'title': 'Fits/blurred vision/headache'
+      'title': 'Fits/blurred vision/headache',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 15,
       'filename': 'urinesugar.mp4',
-      'title': 'Urine Sugar'
+      'title': 'Urine Sugar',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 16,
       'filename': 'fever.mp4',
-      'title': 'Fever'
+      'title': 'Fever',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 17,
       'filename': 'foulsmell.mp4',
-      'title': 'Foul smelling Discharge'
+      'title': 'Foul smelling Discharge',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
     },
     {
       'id': 18,
       'filename': 'weakness.mp4',
-      'title': 'Weakness/other illness'
+      'title': 'Weakness/other illness',
+      'methodName':'Male Sterilization',
+      'methodMsg':'(kaise apnayen purush nasbandi)'
+    }*/
+  ];
+  var childVideos = [
+    {
+      'id': 1,
+      'filename': 'breastfeed.mp4',
+      'title': 'ASHA AWWW VISIT',
+      'methodName':'Breastfeed your baby',
+      'methodMsg':'(Apne shishu ko stanpaan karaein)'
+    },
+    {
+      'id': 2,
+      'filename': 'newborncare.mp4',
+      'title': 'ANM. DOC. CHECKUP',
+      'methodName':'Take care of your newborn',
+      'methodMsg':'(Navjaat shishu ki dekhbaal karein)'
+    },
+    {
+      'id': 3,
+      'filename': 'immunization.mp4',
+      'title': 'Weight',
+      'methodName':'Immunize your child',
+      'methodMsg':'(Bachhe ka Tikakaran karaein)'
+    },
+    {
+      'id': 4,
+      'filename': 'poliodrops.mp4',
+      'title': 'TT',
+      'methodName':'Polio',
+      'methodMsg':'(Bache ko polio ki khurak dilvaaein)'
+    },
+    {
+      'id': 5,
+      'filename': 'vitamina.mp4',
+      'title': 'Anaemia',
+      'methodName':'Vitamin A',
+      'methodMsg':'(Bache ko Vitamin A ki khurak dilvaaein)'
     }
   ];
 
@@ -175,8 +264,21 @@ angular.module('uhiApp.services').factory('videos',function($window){
     return ancVideos;
   };
 
+  var getChildVideos = function() {
+    var list;
+    childVideos.map(function(e) {
+      if(isDevice) {
+        e.path =  baseVideosPath + e.filename;
+      } else {
+        e.path = 'videos/sample.mp4'
+      }
+    });
+    return childVideos;
+  };
+
   return {
     getFamilyPlanningVideos: getFamilyPlanningVideos,
-    getANCVideos : getANCVideos
+    getANCVideos : getANCVideos,
+    getChildVideos : getChildVideos
   }
 });
