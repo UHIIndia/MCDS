d88e29570bbb67cb25daa3bbb28afaa541bbdc1b
Priya Ranjan Singh <mail@priyaranjan.net>
2015-05-12 23:31:55 +0530

    modify template and ctrl to support scroll on device, relates to #2

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,21 +1,23 @@
 angular.module('uhiApp.controllers')
-.controller('LoginController', function($scope, $location) {
+  .controller('LoginController', function($scope, $state) {
 
-    //code for tap on enter button
-    $scope.enterHome = function(){
-        var code = 123;
-        if(code === $scope.workerCode){
-            $scope.required = false;
-            $scope.incorrect = false;
-            $scope.workerCode ="";
-            $location.path("home");
-        }else if( $scope.workerCode === null ||  $scope.workerCode == undefined){
-            $scope.required = true;
-            $scope.incorrect = false;
-        }else{
-            $scope.incorrect = true;
-            $scope.required = false;
-        }
+  $scope.login = {};
+  var code = 123;
+
+  //code for tap on enter button
+  $scope.enterHome = function(){
+    if(code === $scope.login.workerCode){
+      $scope.required = false;
+      $scope.incorrect = false;
+      $scope.login.workerCode = null;
+      $state.go('home');
+    }else if( $scope.login.workerCode === null ||  $scope.login.workerCode == undefined){
+      $scope.required = true;
+      $scope.incorrect = false;
+    }else{
+      $scope.incorrect = true;
+      $scope.required = false;
     }
+  };
 
 });
