05a6994baa21def71405d5df5ab51092453b430a
Mehak Dua <mdua798@gmail.com>
2015-05-14 23:05:53 +0530

    general method for ANC action and add details for display all to MAPWoman

--- www/js/controllers/map-women.js
+++ www/js/controllers/map-women.js
@@ -1,3 +1,42 @@
-angular.module('uhiApp.controllers').controller('MapWomenController', function($scope) {
-
+angular.module('uhiApp.controllers').controller('MapWomenController', function($scope,$state,WomanService,ChildService,UtilityService) {
+ 
+ //get pregnantwoman   
+  var womanList = WomanService.getWomanList();
+    womanList.map(function(e) {
+      var child = ChildService.getChildren(e.womanID);
+      child = child.filter(function(e){         //remove children that are under "miscarriage" category
+        return e.gender != 'misscarriage'
+      })
+     /* child.map(function(e){                          //children for 1 woman
+       e.childAge = UtilityService.calcAge(e.dob)         
+      // e.childAction = ChildService.getImmunizationCategory(e.childId) 
+      })*/
+      if(e.isPregnant === true){
+       e.womanAction = WomanService.getWomanANCDetails(e.displayID)         //action for preg woman 
+       if(child.length == 0){                     //calculate image categ based on woman status and no of children
+        e.imgCategory = 0;
+       }else if(child.length == 1){
+        e.imgCategory = 1;
+       }else if(child.length == 2){
+        e.imgCategory = 2;
+       }else if(child.length > 2){
+        e.imgCategory = 3;
+       }
+      }else{
+       // e.womanAction = WomanService.getFPCategory(e.womanID)         //action for not preg woman  
+       if(child.length == 0){
+        e.imgCategory = 4;
+       }else if(child.length == 1){
+        e.imgCategory = 5;
+       }else if(child.length == 2){
+        e.imgCategory = 6;
+       }else if(child.length > 2){
+        e.imgCategory = 7;
+       }
+      }
+    });
+ 
+  //}
+  console.log(womanList)
+  
 });
--- www/js/services/woman.js
+++ www/js/services/woman.js
@@ -80,6 +80,166 @@ angular.module('uhiApp.services')
       FileService.writeToLocalStorage(womenList, true);
         console.log(womenList);
         return womanID;      
+    },
+    getWomanList :function(){
+      return womenList;
+    },
+    getPregWomanCategory:function(womanId){
+
+      var pregList = womenList.filter(function(e) {
+          return e.isPregnant === true;
+        });
+      console.log(pregList)
+      var pregList = [];
+      var notPregList =[];
+      for(var i=0; i<womenList.length; i++){
+        if(womenList[i].isPregnant == true){
+          var pregObj= {id:womenList[i].displayID};
+          pregList.push(pregObj)
+        }else{
+          var notPregObj= {id:womenList[i].displayID};
+          notPregList.push(notPregObj);
+        }
+      }
+      womanCategory.pregnant = pregList;
+      womanCategory.notPregnant = notPregList;
+      return womanCategory;
+    },
+    getWomanANCDetails:function(displayID){
+      var actionCode =0;
+      var womanData = this.getWomanDetails(displayID);
+      var ANC = womanData.ANC;
+      //womanData.EDD ="12//12/2015";
+      if(ANC != null){
+        var actionStatus =0;
+        var today = new Date();
+        var currentMonth = today.getMonth() + 1;
+        var deliveryDateMonth = UtilityService.showMonthFromDate(womanData.EDD);
+        if(deliveryDateMonth <= 8){
+            deliveryDateMonth =parseInt(deliveryDateMonth)+ 12;
+        }
+        monthNo = deliveryDateMonth - 8;
+        var i;
+        for(i = 1; i <=9;i++){          //start from curentMonth
+            if(monthNo == 13){         //MOVE TO JANUARY
+                monthNo = 1;
+            }
+           if(currentMonth == monthNo){
+            break;
+           }
+            monthNo++;
+        }
+        var pregMon= i;
+
+        //check for anm doc checkup
+        var actionANM = 0;
+        if(ANC[pregMon -1].ANMVisit != null && pregMon !=9){
+          actionANM = 0;
+        }else{
+          actionANM = 1;
+        }
+
+        //check for wt alert
+        var actionCodeWt =0;
+        var lastWt = ANC[pregMon -1].wt;
+        var currentWt = ANC[pregMon].wt;
+        if(lastWt != null){
+          if(lastWt < 40){
+            actionCodeWt =2;
+          }else if(pregMon-1 > 3){
+            for(var j = 1;j<pregMon-1;j++){
+              if(ANC[j].wt != null &&  ANC[j].wt > wtVisits[i].value){
+                actionCodeWt=2;
+              }
+            }
+          }
+        }
+        if(currentWt != null){
+          if(currentWt < 40){
+            actionCodeWt =2;
+          }else if(pregMon > 3){
+            for(var j = 1;j<pregMon;j++){
+              if(ANC[j].wt != null &&  ANC[j].wt > currentWt){
+                actionCodeWt=2;
+              }
+            }
+          }
+        }
+
+        //check for TT    
+        var actionCodeTT =0,thisPregTT =0;
+        var lastPregTT = (ANC[0].TT == null ? 0:parseInt(ANC.TT))
+        for(i=1;i<= pregMon; i++){
+          if(ANC[i].TT != null)
+          thisPregTT += parseInt(ANC[i].TT); 
+        }  
+        if(thisPregTT > 2){             //no action  required
+          actionCodeTT = 0;
+        }else if(thisPregTT == 1  && lastPregTT < 1 || thisPregTT == 0){ 
+          actionCodeTT = 1;                 //action required
+        }
+        
+        //check for Anaemia
+        var actionHB =0;
+        var lastHB= ANC[pregMon-1].HB;
+        var currentHB =ANC[pregMon].HB;
+        if(lastHB != null){
+          if(lastHB < 10){
+            actionHB = 2;
+          }
+        }
+        if(currentHB != null){
+          if(currentHB < 10){
+            actionHB = 2;
+          }
+        }
+        //check for paleEye ,night blind
+        var commonAction =0;
+        var items =['paleEye','nightBlindness','bleeding','malaria','urineProtein','swelling','headache','urineSugar','fever','foulSmellingDischarge','otherInfection']
+        for(var i=0 ;i< items.length;i++ ){
+          var lastValue= ANC[pregMon-1][items[i]];
+          var currentValue =ANC[pregMon][items[i]];
+          if(lastValue == 'yes' || currentValue == 'yes'){  //if  any of the value is red ..then break it
+            commonAction = 2
+            break;
+          }else{
+            commonAction = 0;
+          }
+        }
+
+        //check for IFA
+        var actionIFA=0,totalIFA =0;
+        for(var i=1;i <=pregMon;i++){
+          if(ANC[i].IFA != null){
+            totalIFA += ANC[i].IFA;
+          }
+        }
+        if(totalIFA <100){
+          actionIFA = 1;
+        }
+        //check for BP
+        var actionBP = 0;
+        var lastBPValue= ANC[pregMon-1].BP;
+        var currentBPValue =ANC[pregMon].BP;
+        if(lastValue == 'High' || currentValue == 'High'){  //if  any of the value is red ..then break it
+          actionBP = 2
+        }else{
+          actionBP = 0;
+        }
+
+        //overall check for all checkups
+        if(actionCodeWt == 2 || actionHB ==2 || commonAction ==2 || actionBP ==2){
+          actionCode = 2;
+        }else if(actionANM == 1|| actionCodeTT == 1 || actionIFA == 1){
+          actionCode = 1;
+        }else{
+          actionCode = 0;
+        }
+      }else{
+         actionCode = 1;
+      }
+      return actionCode;
+
     }
   }
 }]);
